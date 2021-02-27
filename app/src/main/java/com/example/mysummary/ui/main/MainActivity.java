5983c2071ab2099dabe3e910b8bd22dedff5e3e1
mohammed mansour <mohammed.mansour4009@gmail.com>
2021-02-27 15:58:26 +0200

    edit for  all  design

--- app/src/main/java/com/example/mysummary/ui/main/MainActivity.java
+++ app/src/main/java/com/example/mysummary/ui/main/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -24,6 +23,7 @@ public class MainActivity extends BaseBindingActivity<ActivityMainBinding> {
         navController = Navigation.findNavController(this, R.id.f_main);
         initNavController();
         initToolbar();
+
 
     }
 
