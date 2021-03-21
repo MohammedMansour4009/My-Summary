a7316dbd93764d24a9cc63e8c962668b57a74373
Jehad Alomour <73334515+JehadEAlomour@users.noreply.github.com>
2021-03-21 15:06:08 +0200

    push work

---
+++ app/src/main/java/com/example/mysummary/ui/fragment/parts/PatrsEngFragment.java
@@ -0,0 +1,47 @@
+package com.example.mysummary.ui.fragment.parts;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.mysummary.databinding.FragmentPatrsEngBinding;
+import com.example.mysummary.model.colleges.Parts;
+
+import java.util.ArrayList;
+
+public class PatrsEngFragment extends Fragment {
+   private FragmentPatrsEngBinding binding;
+   private ArrayList<Parts> parts;
+   private PartsAdapter adapter;
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getRemotePart();
+        initRecyclerViePart();
+
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding=FragmentPatrsEngBinding.inflate(getLayoutInflater());
+
+        return binding.getRoot();
+    }
+private void getRemotePart(){
+        parts=new ArrayList<Parts>();
+    }
+    private void initRecyclerViePart(){
+        adapter=new PartsAdapter(parts);
+        binding.rvParts.setAdapter(adapter);
+
+    }
+
+}
